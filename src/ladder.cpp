2686572d93e5f086a1f218fd6aae0731c5078ee9
seoyeok6 <seoyeok6@uci.edu>
2025-03-09 06:45:48 +0000

    infinite

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -130,7 +130,7 @@ vector<string> generate_word_ladder(const string& begin_word, const string& end_
             }
         }
         
-        if (begin_word == end_word) {
+        if (used_words.empty()) {
             return {};
         }
         // Remove all words used in this level from the dictionary.
@@ -164,7 +164,7 @@ void print_word_ladder(const vector<string>& ladder) {
         return;
     }
 
-    cout << "Word ladder:" << endl;
+    cout << "Word ladder found:";
     for (const string& word : ladder) {
         cout << word << " ";
     }
